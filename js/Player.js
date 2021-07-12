6bb251dd017ef4f3b90c88f38016657cff9e5fa1
Anushka2006 <77450251+Anushka2006@users.noreply.github.com>
2021-07-12 20:45:48 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,56 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.playerRank=0; 
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+     
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+
+  getCarsAtEnd(){
+    database.ref('carsAtEnd').on("value",(data)=>{
+    this.playerRank= data.val();
+
+    }
+    );
+
+
+  }
+
+  static updateCarsAtEnd(playerRank){
+   //called by class, for the entire class
+   var playerRankref = database.ref('/');
+   playerRankref.update({
+     carsAtEnd: playerRank
+   });
+
+  }
+}
